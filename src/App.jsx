9ad2506000fabad3fa18df3acb4b38d797c755c6
Thirualpha <tthirumurugan802@gmail.com>
2023-09-07 14:53:10 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FetchComponent from './assets/FetchComponent/FetchComponent'
+import './App.css'
+function App() {
+  return (
+    <React.Fragment>
+      <FetchComponent/>
+    </React.Fragment>
+  )
+}
+
+export default App
